abbac41be0b6b3febcb30af30a35758eed2deb51
Danny van Kooten <dannyvankooten@gmail.com>
2016-06-20 19:21:24 +0200

    Export `FetchRates()` func so this can be called at first day of month (for example) to get latest VAT rates. Closes #1

--- rates.go
+++ rates.go
@@ -7,9 +7,11 @@ import (
 	"time"
 )
 
+// RatePeriod represents a time and the various activate rates at that time.
 type RatePeriod struct {
-	EffectiveFrom string `json:"effective_from"`
-	Rates         map[string]float32
+	EffectiveFromStr string `json:"effective_from"`
+	EffectiveFrom    time.Time
+	Rates            map[string]float32
 }
 
 // CountryRates holds the various differing VAT rate periods for a given country
@@ -20,13 +22,7 @@ type CountryRates struct {
 	Periods     []RatePeriod
 }
 
-type apiResponse struct {
-	Details string
-	Version string
-	Rates   []CountryRates
-}
-
-var countryRates []CountryRates
+var countriesRates []CountryRates
 
 // ErrInvalidCountryCode will be returned when calling GetCountryRates with an invalid country code
 var ErrInvalidCountryCode = errors.New("Unknown country code.")
@@ -37,14 +33,11 @@ var ErrInvalidRateLevel = errors.New("Unknown rate level")
 // RateOn returns the effective VAT rate on a given date
 func (cr *CountryRates) RateOn(t time.Time, level string) (float32, error) {
 	var activePeriod RatePeriod
-	var activePeriodDate time.Time
 
 	// find active period for the given time
 	for _, p := range cr.Periods {
-		periodDate, _ := time.Parse("2006-01-01", p.EffectiveFrom)
-		if t.After(periodDate) && periodDate.After(activePeriodDate) {
+		if t.After(p.EffectiveFrom) && p.EffectiveFrom.After(activePeriod.EffectiveFrom) {
 			activePeriod = p
-			activePeriodDate = periodDate
 		}
 	}
 
@@ -64,8 +57,8 @@ func (cr *CountryRates) Rate(level string) (float32, error) {
 
 // GetCountryRates gets the CountryRates struct for a country by its ISO-3166-1-alpha2 country code.
 func GetCountryRates(c string) (CountryRates, error) {
-	rates, err := fetch()
 	var rate CountryRates
+	rates, err := GetRates()
 
 	if err != nil {
 		return rate, err
@@ -80,19 +73,41 @@ func GetCountryRates(c string) (CountryRates, error) {
 	return rate, ErrInvalidCountryCode
 }
 
-func fetch() ([]CountryRates, error) {
-	if countryRates != nil {
-		return countryRates, nil
+// GetRates returns the in-memory VAT rates
+func GetRates() ([]CountryRates, error) {
+	var err error
+
+	if countriesRates == nil {
+		countriesRates, err = FetchRates()
 	}
+
+	return countriesRates, err
+}
+
+// FetchRates fetches the latest VAT rates from jsonvat.com and updates the in-memory rates
+func FetchRates() ([]CountryRates, error) {
 
 	r, err := http.Get("https://jsonvat.com/")
 	if err != nil {
 		return nil, err
 	}
 
-	data := new(apiResponse)
-	err = json.NewDecoder(r.Body).Decode(&data)
-	countryRates = data.Rates
+	type CR CountryRates
+	apiResponse := &struct {
+		Details string
+		Version string
+		Rates   []CountryRates
+	}{}
 
-	return countryRates, err
+	err = json.NewDecoder(r.Body).Decode(&apiResponse)
+
+	// convert EffectiveFrom to a proper time.Time for each rate period
+	for idx1, cr := range apiResponse.Rates {
+		for idx2, crp := range cr.Periods {
+			apiResponse.Rates[idx1].Periods[idx2].EffectiveFrom, _ = time.Parse("2006-01-01", crp.EffectiveFromStr)
+		}
+	}
+
+	countriesRates = apiResponse.Rates
+	return countriesRates, err
 }
