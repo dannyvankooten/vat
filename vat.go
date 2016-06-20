3b2e6be2c1e8392261098e2df24579db8e30dfa0
Danny van Kooten <dannyvankooten@gmail.com>
2016-06-20 19:38:48 +0200

    Fix example in godoc

--- vat.go
+++ vat.go
@@ -4,7 +4,7 @@ Package vat helps you deal with European VAT in Go.
 It offers VAT number validation using the VIES VAT validation API & VAT rates retrieval using jsonvat.com
 
 Validate a VAT number
-		validity := vat.Validate("NL123456789B01")
+		validity := vat.ValidateNumber("NL123456789B01")
 
 Get VAT rate that is currently in effect for a given country
 		c, _ := vat.GetCountryRates("NL")
